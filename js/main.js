05caaafd634d61f3707c883500ed7f5ed58a9f69
Илья <ilya-best85@yandex.ru>
2021-09-25 17:38:51 +0300

    fix errors

--- js/main.js
+++ js/main.js
@@ -1,19 +1,12 @@
 function getRandomNumber (min, max) {
   if(min<max && min>=0){
-    const arr = [];
-    for(let shetchik=0; shetchik <=(max-min); shetchik++){
-      arr[shetchik]=[Math.random(), shetchik+min];
-    }
-    return arr.sort()[0][1];
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min;
   }
-
 }
 
-function chekComment(comUser, maxLength) {
-  const lengthString = comUser.length;
-  if(lengthString <= maxLength){return true;}
-  return false;
-}
+const chekComment = (comment, maxLength) => comment.length <= maxLength;
 
 getRandomNumber(0, 5); //ссылка на функцию https://efim360.ru/javascript-sluchajnoe-czeloe-chislo-iz-diapazona/
-chekComment('first comment', 14);
+chekComment('volvo', 5);
