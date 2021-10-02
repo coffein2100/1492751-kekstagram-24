d10dece496386f507746da08ab9d9721f2662075
Илья <ilya-best85@yandex.ru>
2021-10-03 00:17:25 +0300

    work

--- js/main.js
+++ js/main.js
@@ -6,15 +6,20 @@ function getRandomNumber (min, max) {
   }
 }
 
+// eslint-disable-next-line no-unused-vars
 const chekComment = (comment, maxLength) => comment.length <= maxLength;
 
-getRandomNumber(0, 5);
-chekComment('volvo', 5);
+const usedIndexes = new Set();
+function getUniqueRandomNumber (max, min) {
+  const newNumber = Math.floor (Math.random() * (max - min) + min);
+  if (usedIndexes.has(newNumber)) {
+    return this.getUniqueRandomNumber(max, min);
+  } else {
+    usedIndexes.add(newNumber);
+    return newNumber;
+  }
+}
 
-/*Должно быть что-то типа такого объекта для 1 поста:
-*/
-
-const SOMEIDS = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,20,22,23,24,25];
 const NAMES = [
   'Иван',
   'Мария',
@@ -38,34 +43,36 @@ const DESKRIPTIONS = [
   'за секунду до',
 
 ];
-const SIMILAR_IMAGES_COUNT = 25;
+const SIMILAR_IMAGES_COUNT = 5;
+const SIMILAR_COMMENTS_COUNT = 3;
 
-const createImege = () => {
+const createComments = () => {
   const randomNameIndex = _.random(0, NAMES.length - 1);
-  const randomSomeIndex = _.random(0, SOMEIDS.length - 1);
-  const randomDeskriptionIndex = _.random(0, DESKRIPTIONS.length - 1);
-  const SomeUrl = `photos/${  _.random(0, SOMEIDS.length - 1)  }.jpg`;
-  const SomeAvatar = `img/avatar-${  getRandomNumber(1, 6)  }.svg`;
-  const someLikes = getRandomNumber(15, 200);
   const randomCommentIndex = _.random(0, COMMENTS.length - 1);
-  const someComment1Id = getRandomNumber (1, 1000000);
-  const someComment2Id = getRandomNumber (1, 1000000);
+  const SomeAvatar = `img/avatar-${  getRandomNumber(1, 6)  }.svg`;
+  const uniqueIdComennts = getUniqueRandomNumber (500, 1);
   return {
-    id:SOMEIDS[randomSomeIndex],
-    url:SomeUrl,
-    description:DESKRIPTIONS[randomDeskriptionIndex],
-    likes: someLikes,
-    comments: [{
-      id: someComment1Id, // это id 1го коммента для этого поста
-      avatar: SomeAvatar,
-      message: COMMENTS[randomCommentIndex],
-      name: NAMES[randomNameIndex],
-    }, {
-      id: someComment2Id, // это id 2го коммента для этого поста
-      avatar: SomeAvatar,
-      message: COMMENTS[randomCommentIndex],
-      name: NAMES[randomNameIndex],
-    }],
+    id: uniqueIdComennts,
+    avatar: SomeAvatar,
+    message: COMMENTS[randomCommentIndex],
+    name: NAMES[randomNameIndex],
   };
 };
+const similarComments = Array.from({length:SIMILAR_COMMENTS_COUNT}, createComments);
+
+const createImege = () => {
+  const randomDeskriptionIndex = _.random(0, DESKRIPTIONS.length - 1);
+  const someLikes = getRandomNumber(15, 200);
+  const unique = getUniqueRandomNumber (25, 1);
+  return {
+    id:unique,
+    url:`photos/${ unique  }.jpg`,
+    description:DESKRIPTIONS[randomDeskriptionIndex],
+    likes: someLikes,
+    comments:similarComments,
+  };
+};
+
 const similarImages = Array.from({length: SIMILAR_IMAGES_COUNT}, createImege);
+
+console.log(similarImages);
