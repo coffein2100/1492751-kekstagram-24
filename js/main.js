84a65f47d571d62866353dcc14fe898e507a4f7c
Илья <ilya-best85@yandex.ru>
2021-10-03 11:06:42 +0300

    work dz, correction

--- js/main.js
+++ js/main.js
@@ -9,17 +9,6 @@ function getRandomNumber (min, max) {
 // eslint-disable-next-line no-unused-vars
 const chekComment = (comment, maxLength) => comment.length <= maxLength;
 
-const usedIndexes = new Set();
-function getUniqueRandomNumber (max, min) {
-  const newNumber = Math.floor (Math.random() * (max - min) + min);
-  if (usedIndexes.has(newNumber)) {
-    return this.getUniqueRandomNumber(max, min);
-  } else {
-    usedIndexes.add(newNumber);
-    return newNumber;
-  }
-}
-
 const NAMES = [
   'Иван',
   'Мария',
@@ -33,6 +22,7 @@ const COMMENTS = [
   'Всё отлично!',
   'В целом всё неплохо. Но не всё.',
   'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
 ];
 const DESKRIPTIONS = [
   'красивый вид',
@@ -41,38 +31,35 @@ const DESKRIPTIONS = [
   'закат на берегу моря',
   'Новый рекорд по бегу',
   'за секунду до',
-
 ];
-const SIMILAR_IMAGES_COUNT = 5;
+const SIMILAR_IMAGES_COUNT = 25;
 const SIMILAR_COMMENTS_COUNT = 3;
 
-const createComments = () => {
+const createComments = (position) => {
   const randomNameIndex = _.random(0, NAMES.length - 1);
   const randomCommentIndex = _.random(0, COMMENTS.length - 1);
   const SomeAvatar = `img/avatar-${  getRandomNumber(1, 7)  }.svg`;
-  const uniqueIdComennts = getUniqueRandomNumber (500, 1);
   return {
-    id: uniqueIdComennts,
+    id: position+1,
     avatar: SomeAvatar,
     message: COMMENTS[randomCommentIndex],
     name: NAMES[randomNameIndex],
   };
 };
-const similarComments = Array.from({length:SIMILAR_COMMENTS_COUNT}, createComments);
+const similarComments = Array.from({length:SIMILAR_COMMENTS_COUNT},(_id,position) => createComments(position));
 
-const createImege = () => {
+const createImege = (position) => {
   const randomDeskriptionIndex = _.random(0, DESKRIPTIONS.length - 1);
   const someLikes = getRandomNumber(15, 201);
-  const unique = getUniqueRandomNumber (25, 1);
+
   return {
-    id:unique,
-    url:`photos/${ unique  }.jpg`,
+    id: position+1,
+    url:`photos/${position+1}.jpg`,
     description:DESKRIPTIONS[randomDeskriptionIndex],
     likes: someLikes,
     comments:similarComments,
   };
 };
 
-const similarImages = Array.from({length: SIMILAR_IMAGES_COUNT}, createImege);
-
-console.log(similarImages);
+// eslint-disable-next-line no-unused-vars
+const similarImages = Array.from({length: SIMILAR_IMAGES_COUNT},(_id,position) => createImege(position));
