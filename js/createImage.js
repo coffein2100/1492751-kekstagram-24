e4ea14bc3d8d91bc31979f91f96c52aca7ef737f
Илья <ilya-best85@yandex.ru>
2021-10-12 23:58:01 +0300

    correction

--- js/createImage.js
+++ js/createImage.js
@@ -7,15 +7,17 @@ const similarImageTemplate = document.querySelector('#picture')
   .querySelector('.picture');
 
 const imageElements = similarImages;
+function createImage() {
+  const similarImageFragment = document.createDocumentFragment();
 
-const similarImageFragment = document.createDocumentFragment();
+  imageElements.forEach((image) => {
+    const imageElement = similarImageTemplate.cloneNode(true);
+    imageElement.querySelector('.picture__comments').textContent = image.comments.length;
+    imageElement.querySelector('.picture__likes').textContent = image.likes;
+    imageElement.querySelector('.picture__img').src = image.url;
+    similarImageFragment.appendChild(imageElement);
+  });
+  randomPicture.appendChild(similarImageFragment);
+}
 
-imageElements.forEach((image) => {
-  const imageElement = similarImageTemplate.cloneNode(true);
-  imageElement.querySelector('.picture__comments').textContent = image.comments.length;
-  imageElement.querySelector('.picture__likes').textContent = image.likes;
-  imageElement.querySelector('.picture__img').removeAttribute('src');
-  imageElement.querySelector('.picture__img').setAttribute('src', image.url);
-  similarImageFragment.appendChild(imageElement);
-});
-randomPicture.appendChild(similarImageFragment);
+createImage(similarImages);
