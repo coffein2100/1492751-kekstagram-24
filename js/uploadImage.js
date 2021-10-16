c519593cd5316ab4c5b4128a68e7047964a6eb11
Илья <ilya-best85@yandex.ru>
2021-10-16 18:55:23 +0300

    dz

--- js/uploadImage.js
+++ js/uploadImage.js
@@ -1,2 +1,7 @@
 const formImage = document.querySelector('.img-upload__form');
+const imgLoad = document.querySelector('.img-upload__overlay');
 
+
+formImage.addEventListener('change',() =>{
+  imgLoad.classList.remove('hidden');
+});
