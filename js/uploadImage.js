bb5ab0b233f50d76150fef46391509826586bef0
Илья <ilya-best85@yandex.ru>
2021-10-23 14:07:53 +0300

    works

--- js/uploadImage.js
+++ js/uploadImage.js
@@ -12,34 +12,30 @@ const MAX_COMMENT_LENGTH = 140;
 
 const chekHashtags = () => {
   let  arrayHashtag = hashtags.value.toLowerCase();
+  const usedHashtag = new Set();
   arrayHashtag=arrayHashtag.split(' ');
   if (arrayHashtag.length >5){
     hashtags.setCustomValidity('нельзя указать больше пяти хэш-тегов');
   }
-
   else {
     hashtags.setCustomValidity('');
   }
-  /*hashtags.setCustomValidity(`
-  +хэш-тег начинается с символа # (решётка);
-  строка после решётки должна состоять из букв и чисел и не может содержать пробелы, спецсимволы (#, @, $ и т. п.), символы пунктуации (тире, дефис, запятая и т. п.), эмодзи и т. д.;
-  +хеш-тег не может состоять только из одной решётки;
-  +максимальная длина одного хэш-тега 20 символов, включая решётку;
-  хэш-теги нечувствительны к регистру: #ХэшТег и #хэштег считаются одним и тем же тегом;
-  хэш-теги разделяются пробелами;
-  один и тот же хэш-тег не может быть использован дважды;
-  +нельзя указать больше пяти хэш-тегов;`);*/
+  arrayHashtag.forEach((value) => {
+    if (usedHashtag.has(value)){hashtags.setCustomValidity('хэш-теги нечувствительны к регистру: #ХэшТег и #хэштег считаются одним и тем же тегом. Oдин и тот же хэш-тег не может быть использован дважды');}
+    else {usedHashtag.add(value);}
+  });
+
   for (let i=0;i<arrayHashtag.length;i++){
+    const format = /[ `!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/;
     arrayHashtag[i].split('');
     if (!arrayHashtag[i].startsWith('#')){hashtags.setCustomValidity('хэш-тег начинается с символа # (решётка)');}
-    else if(arrayHashtag[i] === arrayHashtag){hashtags.setCustomValidity('один и тот же хэш-тег не может быть использован дважды');}
     else if (arrayHashtag[i].length>20 ){hashtags.setCustomValidity('максимальная длина одного хэш-тега 20 символов, включая решётку');}
     else if (arrayHashtag[i].length < 2 ) {hashtags.setCustomValidity('хеш-тег не может состоять только из одной решётки');}
+    arrayHashtag[i]= arrayHashtag[i].substr(1);
+    if (format.test(arrayHashtag[i])) {hashtags.setCustomValidity('строка после решётки должна состоять из букв и чисел и не может содержать пробелы, спецсимволы (#, @, $ и т. п.), символы пунктуации (тире, дефис, запятая и т. п.), эмодзи и т. д.;');}
   }
   hashtags.reportValidity();
-
 };
-
 
 const chekComment = () => {
   const commentLength = comment.value.length;
@@ -54,7 +50,7 @@ const chekComment = () => {
 };
 
 
-const minSize = () =>{
+const reduceSize = () =>{
   let scaleSize = scaleImage.querySelector('.scale__control--value').value;
   scaleSize = parseFloat(scaleSize);
   if (scaleSize > 25) {
@@ -63,7 +59,7 @@ const minSize = () =>{
     sizeImg.style.transform = `scale(${scaleSize/100})`;
   }
 };
-const maxSize = () =>{
+const increaseSize = () =>{
   let scaleSize = scaleImage.querySelector('.scale__control--value').value;
   scaleSize = parseFloat(scaleSize);
   if (scaleSize < 100) {
@@ -85,12 +81,6 @@ const clearForm = () => {
   sizeImg.className = 'effects__preview--none';
 };
 
-const sendForm = (evt) => {
-  evt.prevent.Default();
-  // Очищаем поля формы
-  sizeImg.style.transform = 'scale(1)';
-  evt.target.reset();
-};
 const keyDownFormImage = (evt) => {
   if (evt.keyCode === 27) {
     if (document.activeElement === formImage.querySelector('.text__hashtags') ||
@@ -99,6 +89,14 @@ const keyDownFormImage = (evt) => {
     } else {
       modalView.classList.remove('modal-open');
       imgLoad.classList.add('hidden');
+      // eslint-disable-next-line no-use-before-define
+      closeButton.removeEventListener('click', onCloseClick);
+      formImage.removeEventListener('keydown', keyDownFormImage);
+      scaleImage.querySelector('.scale__control--smaller').removeEventListener('click', reduceSize);
+      scaleImage.querySelector('.scale__control--bigger').removeEventListener('click', increaseSize);
+      comment.removeEventListener('input', chekComment);
+      hashtags.removeEventListener('input', chekHashtags);
+      clearForm();
     }
   }
 };
@@ -108,10 +106,9 @@ const onCloseClick = () => {
   imgLoad.classList.add('hidden');
   closeButton.removeEventListener('click', onCloseClick);
   formImage.removeEventListener('keydown', keyDownFormImage);
-  scaleImage.querySelector('.scale__control--smaller').removeEventListener('click', minSize);
-  scaleImage.querySelector('.scale__control--bigger').removeEventListener('click', maxSize);
+  scaleImage.querySelector('.scale__control--smaller').removeEventListener('click', reduceSize);
+  scaleImage.querySelector('.scale__control--bigger').removeEventListener('click', increaseSize);
   formImage.removeEventListener('change', onFilterChange);
-  formImage.removeEventListener('submit', sendForm);
   comment.removeEventListener('input', chekComment);
   hashtags.removeEventListener('input', chekHashtags);
   clearForm();
@@ -120,10 +117,9 @@ const onCloseClick = () => {
 const openUploadForm = () => {
   closeButton.addEventListener('click', onCloseClick);
   formImage.addEventListener('keydown', keyDownFormImage);
-  scaleImage.querySelector('.scale__control--smaller').addEventListener('click', minSize);
-  scaleImage.querySelector('.scale__control--bigger').addEventListener('click', maxSize);
+  scaleImage.querySelector('.scale__control--smaller').addEventListener('click', reduceSize);
+  scaleImage.querySelector('.scale__control--bigger').addEventListener('click', increaseSize);
   formImage.addEventListener('change', onFilterChange);
-  formImage.addEventListener('submit', sendForm);
   comment.addEventListener('input', chekComment);
   hashtags.addEventListener('input', chekHashtags);
 };
@@ -134,89 +130,3 @@ const formChange = () => {
 };
 
 formImage.addEventListener('change', formChange);
-
-/*const formChange = formImage.addEventListener('change',() =>{
-  imgLoad.classList.remove('hidden');
-  modalView.classList.add('modal-open');
-});
-
-const onClickcloseButton = closeButton.addEventListener('click',  (evt) => {
-  evt.preventDefault();
-  modalView.classList.remove('modal-open');
-  imgLoad.classList.add('hidden');
-});
-
-const keyDownFormImage = formImage.addEventListener('keydown', (evt) => {
-  if (evt.keyCode === 27) {
-    evt.preventDefault();
-    if (document.activeElement === formImage.querySelector('.text__hashtags') ||
-    document.activeElement === formImage.querySelector('.text__description')){
-      return; } else {
-      modalView.classList.remove('modal-open');
-      imgLoad.classList.add('hidden');
-      formImage.removeEventListener('click', keyDownFormImage);
-    }
-  }
-});
-closeButton.addEventListener('keydown', keyDownFormImage);
-
-scaleImage.querySelector('.scale__control--smaller').addEventListener('click',() =>{
-  let scaleSize = scaleImage.querySelector('.scale__control--value').value;
-  scaleSize = parseFloat(scaleSize);
-  if (scaleSize > 25) {
-    scaleSize -=25;
-    scaleImage.querySelector('.scale__control--value').value = `${scaleSize}%`;
-    sizeImg.style.transform = `scale(${scaleSize/100})`;
-  }
-});
-scaleImage.querySelector('.scale__control--bigger').addEventListener('click',() =>{
-  let scaleSize = scaleImage.querySelector('.scale__control--value').value;
-  scaleSize = parseFloat(scaleSize);
-  if (scaleSize < 100) {
-    scaleSize +=25;
-    scaleImage.querySelector('.scale__control--value').value = `${scaleSize}%`;
-    sizeImg.style.transform = `scale(${scaleSize/100})`;
-  }
-});
-
-const onFilterChange = (evt) =>{
-  if (evt.target.matches('input[type="radio"]')) {
-    const filterName = evt.target.value;
-    console.log(filterName);
-    // eslint-disable-next-line no-useless-concat
-    sizeImg.className = 'effects__preview--' + `${filterName}`;
-    if (formImage.querySelector('img').classList.contains('effects__preview--chrome')) {formImage.querySelector('img').classList.remove('effects__preview--chrome');}
-    if  (formImage.querySelector('img').classList.contains('effects__preview--sepia')) {formImage.querySelector('img').classList.remove('effects__preview--sepia');}
-    if  (formImage.querySelector('img').classList.contains('effects__preview--marvin')) {formImage.querySelector('img').classList.remove('effects__preview--marvin');}
-    if  (formImage.querySelector('img').classList.contains('effects__preview--phobos')) {formImage.querySelector('img').classList.remove('effects__preview--phobos');}
-    if  (formImage.querySelector('img').classList.contains('effects__preview--heat'))  {formImage.querySelector('img').classList.remove('effects__preview--heat');}
-
-    if (filterName === 'chrome'){
-      formImage.querySelector('img').classList.add('effects__preview--chrome');
-    }
-    if (filterName === 'sepia'){
-      formImage.querySelector('img').classList.add('effects__preview--sepia');
-    }
-    if (filterName === 'marvin'){
-      formImage.querySelector('img').classList.add('effects__preview--marvin');
-    }
-    if (filterName === 'phobos'){
-      formImage.querySelector('img').classList.add('effects__preview--phobos');
-    }
-    if (filterName === 'heat'){
-      formImage.querySelector('img').classList.add('effects__preview--heat');
-    }
-    if (filterName === 'none'){
-      formImage.querySelector('img').classList.remove();
-    }
-
-  }
-};
-
-
-formImage.addEventListener('submit', (e) => {
-  // Отключаем событие по умолчанию
-  e.prevent.Default();
-  // Очищаем поля формы
-  e.target.reset();
-});*/
