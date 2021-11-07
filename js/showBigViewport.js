d84b5c9a9b890a30ca1b529a584559a563f81207
Илья <ilya-best85@yandex.ru>
2021-11-07 21:01:59 +0300

    works

--- js/showBigViewport.js
+++ js/showBigViewport.js
@@ -5,57 +5,6 @@ export const comentsbutton = document.querySelector('.comments-loader');
 const modalView = document.querySelector('body');
 const closeButton = document.querySelector('.big-picture__cancel');
 
-/*
-export const renderBigPicture = (bigImage) => {
-  bigPicturecomment.innerHTML = '';
-  description.textContent = bigImage.description;
-  comments.textContent = bigImage.comments.length;
-  likes.textContent = bigImage.likes;
-  largeImage.src = bigImage.url;
-  const arrayComments = bigImage.comments.slice(0, quantity);
-  const bigImageFragment = document.createDocumentFragment();
-  if (quantity>=comments.textContent){
-    quantity=comments.textContent;
-    socialComentsView.textContent = `${quantity}`;
-    comentsbutton.classList.add('hidden');
-  }
-  arrayComments.forEach((comment) => {
-
-    const element = document.createElement('li');
-    const image = document.createElement('img');
-    const text = document.createElement('p');
-    image.classList.add('social__picture');
-    element.classList.add('social__comment');
-    text.classList.add('social__text');
-    image.src = comment.avatar;
-    image.alt = comment.name;
-    image.width = 35;
-    image.height = 35;
-    text.textContent = comment.message;
-    element.appendChild(image);
-    element.appendChild(text);
-    bigImageFragment.appendChild(element);
-  });
-  bigPicturecomment.appendChild(bigImageFragment);
-};
-const showMoreComments = () => {
-  quantity+=5;
-  if (quantity> comments.textContent){
-    quantity=comments.textContent;
-    comentsbutton.classList.add('hidden');
-  }
-  renderBigPicture(currentPictureData[currentChosenIndex]);
-  socialComentsView.textContent = `${quantity}`;
-};
-export const closeBigPicture = () => {
-  modalView.classList.remove('modal-open');
-  bigPicture.classList.add('hidden');
-  quantity=5;
-  socialComentsView.textContent = `${quantity}`;
-  comentsbutton.classList.remove('hidden');
-  comentsbutton.removeEventListener('click', showMoreComments);
-  closeButton.removeEventListener('click', closeBigPicture);
-};*/
 export const escCloseBigPicture = (evt) => {
   if (evt.code === ESCAPE_BUTTON) {
     closeBigPicture();
